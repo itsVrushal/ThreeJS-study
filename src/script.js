715909e5024f75599cfbb2ad34444d5ad2e98d01
Vrushal Patil <122393568+vrushalpatil01@users.noreply.github.com>
2026-01-19 19:29:01 +0530

    feat: update scene setup with new geometries and lighting

--- src/script.js
+++ src/script.js
@@ -1,21 +1,55 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { materialAO } from "three/tsl";
+import { PointLight } from "three/webgpu";
 
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: "red",
-  wireframe: true,
-});
+const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const Material = new THREE.MeshLambertMaterial();
+// Material.color = new THREE.Color("blue");
+// Material.transparent = true;
+// Material.opacity = 0.5;
+Material.side = 2;
 
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+// const fog = new THREE.Fog(0xffffff, 1, 10);
+// scene.fog = fog;
+// scene.background = new THREE.Color("white");
+
+const cubeMesh = new THREE.Mesh(cubeGeometry, Material);
+
+const Mesh2 = new THREE.Mesh(cubeGeometry, Material);
+Mesh2.position.x = 1.5;
+
+const plane = new THREE.Mesh(planeGeometry, Material);
+plane.position.x = -1.5;
+
+// const geometry = new THREE.SphereGeometry(1, 16, 16);
 // const tempVector = new THREE.Vector3(0, 0, 0);
 
+//Create custom geometry using bufer geometry
+// const verticies = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0]);
+// const bufferAttribute = new THREE.BufferAttribute(verticies, 3);
+// const geometry = new THREE.BufferGeometry();
+// geometry.setAttribute("position", bufferAttribute);
+// const cubeMesh = new THREE.Mesh(geometry, cubeMaterial);
+
 scene.add(cubeMesh);
+scene.add(Mesh2);
+scene.add(plane);
+
+// init Light
+const light = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(light);
+
+const pointLight = new THREE.PointLight(0xffffff, 200);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
 // cubeMesh.position.copy(tempVector);
-const axisHelper = new THREE.AxesHelper(2);
-cubeMesh.add(axisHelper);
+// const axisHelper = new THREE.AxesHelper(2);
+// cubeMesh.add(axisHelper);
 
 // cubeMesh.rotation.y = 2;
 
@@ -57,9 +91,9 @@ const renderloop = () => {
 
   console.log(delta);
 
-  cubeMesh.rotation.y += THREE.MathUtils.degToRad(2) * delta * 20;
+  // cubeMesh.rotation.y += THREE.MathUtils.degToRad(2) * delta * 20;
   // cubeMesh.position.x += THREE.MathUtils.degToRad(1) * delta * 20;
-  cubeMesh.scale.x = 2 + Math.sin(currentTime) * 0.5;
+  // cubeMesh.scale.x = 2 + Math.sin(currentTime) * 0.5;
   // cubeMesh.position.y = 1 + Math.sin(currentTime) * 0.5;
   previousTime = currentTime;
   controls.update();
